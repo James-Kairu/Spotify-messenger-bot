5ddb0c530a7d3548ec288af2ab4dc1d5cffe77ff
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-04-11 04:12:51 +0100

    Working Spotify Search Engine within Chatbot

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
@@ -7,7 +8,7 @@ import ChatBot from 'react-simple-chatbot';
 import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
 
-class Spotify extends Component{
+class SearchTracks extends Component{
   constructor(){
     super();
     const params = this.getHashParams();
@@ -17,7 +18,86 @@ class Spotify extends Component{
     }
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      query:{name:null},
+      Trackname:{name:null},
+    }
+  }
+  componentWillMount() {
+  //console.log(this.props.previousStep["value"]);
+  var workingvalue=this.props.previousStep["value"];
+  //const { steps } = this.props.previousStep["value"];
+  //const { parameter } = this.props.previousStep["value"];
+  console.log(workingvalue);
+
+  this.setState({ query:{name:workingvalue} });
+}
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  searchTracks(query){
+    spotifyApi.searchTracks(query)
+  .then((response) => {
+    this.setState({
+      Trackname: {
+          name: response.tracks.items[0].name,
+        }
+    });
+    //console.log(this.state.query.name);
+    console.log('Search by "Love"', response.tracks.items[0].name);
+  }, function(err) {
+    console.error(err);
+  });
+
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <a href='http://localhost:8888' > Login to Spotify </a>
+        <div>
+          Track Name: { this.state.Trackname.name }
+        </div>
+        { this.state.loggedIn &&
+          <button onClick={() => this.searchTracks(this.state.query.name)}>
+            Check Track Name
+          </button>
+        }
+      </div>
+    );
+  }
+
+}
+
+SearchTracks.propTypes = {
+  steps: PropTypes.object,
+};
+
+SearchTracks.defaultProps = {
+  steps: undefined,
+};
+
+
+class NowPlaying extends Component{
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
     }
   }
   getHashParams() {
@@ -33,7 +113,6 @@ class Spotify extends Component{
   }
 
   getNowPlaying(){
-    console.log("Jumba Jumba");
     spotifyApi.getMyCurrentPlayingTrack()
       .then((response) => {
         this.setState({
@@ -102,7 +181,7 @@ class App extends Component {
               },
               {
                 id: '5',
-                component: <Spotify />,
+                component: <NowPlaying />,
                 trigger: '6',
               },
               {
@@ -112,19 +191,36 @@ class App extends Component {
               },
               {
                 id: '7',
-                user: true,
-                validator: (value) => {
-                  if (value==="yes") {
-                    return true;
-                  }
-                  else{
-                    return 'value should be yes';
-                  }
-                },
-                trigger: '5',
+                options: [
+                    { value: 'yes', label: 'yes', trigger: '5' },
+                    { value: 'no', label: 'no', trigger: 'tracksearch' },
+                  ],
               },
               {
-                id: '8',
+                id: 'tracksearch',
+                options: [
+                    { value: 'yes', label: 'Yes', trigger: '9' },
+                    { value: 'no', label: 'No', trigger: '12' },
+                  ],
+              },
+              {
+                id: '9',
+                message: 'Let me know what track you want to find?',
+                trigger:10,
+              },
+              {
+                id: '10',
+                user: true,
+                trigger:11,
+              },
+              {
+                id: '11',
+                component: <SearchTracks />,
+                waitAction: true,
+                trigger:12,
+              },
+              {
+                id: '12',
                 message: 'Thanks for asking',
                 end: true,
               },
